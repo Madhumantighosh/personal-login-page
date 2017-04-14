5a23f088c925f8881b3e2fee3a70d94efb35c4b6
Madhumanti Ghosh <riya1996ghosh@gmail.com>
2017-04-14 05:54:54 +0100

    Commit message

---
+++ main1.js
@@ -0,0 +1,15 @@
+function check(){
+var Username=document.getElementById('usr').value;
+    var password=document.getElementById('psd').value;
+	if(Username==""){
+		alert('Username is not entered');
+     }
+     else if (password=="")
+      {
+     	alert('password is not entered');
+     } 
+	else{
+	alert("Welcome");
+     }
+
+}
